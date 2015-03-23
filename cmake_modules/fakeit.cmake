fa8cd21ce59aa3720a4a4596d7e4557d6a138de5
Hans Meyer <hamster.of.dev@gmail.com>
2015-03-23 21:04:47 +0100

    :sunrise: Introducing means for testing
    I decided for FakeIt for mocking and Catch for test descriptions.

---
+++ cmake_modules/fakeit.cmake
@@ -0,0 +1,20 @@
+project(fakeit_builder CXX)
+find_package(Git REQUIRED)
+include(ExternalProject)
+
+ExternalProject_Add(
+    fakeit
+    PREFIX ${CMAKE_BINARY_DIR}/fakeit
+    GIT_REPOSITORY https://github.com/eranpeer/FakeIt.git
+    UPDATE_COMMAND ${GIT_EXECUTABLE} pull
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+)
+
+
+ExternalProject_Get_Property(fakeit source_dir)
+set(FAKEIT_FOLDERS ${source_dir}/include/ ${source_dir}/config/standalone/)
+set(FAKEIT_INCLUDE_DIR ${FAKEIT_FOLDERS} CACHE INTERNAL "Path to include folder for FakeIt")
+
