750fe677f443cd847a765856c890771136bd00df
Hans Meyer <hamster.of.dev@gmail.com>
2015-04-15 21:53:53 +0200

    Script improvements for the boost installation
    Users will now see what is done on Linux installations

--- scripts/get-boost.sh
+++ scripts/get-boost.sh
@@ -4,11 +4,11 @@ os="$(uname -s)"
 
 case $os in
     "Linux")
+        echo "Downloading boost from official sources"
         wget -q -O boost.tar.gz http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz/download  \
-            && mkdir boost \
-            && tar --strip-components=1 -C boost -xzf boost.tar.gz \
-            && cd .. \
-            && sudo boost/bootstrap.sh # let boost do its install routine
+            && echo "unpacking to ./boost/ (will be deleted afterwards)" && mkdir boost \
+            && tar --strip-components=1 boost -xzf boost.tar.gz \
+            && echo "Bootstrapping boost" && sudo boost/bootstrap.sh # let boost do its install routine
         rm -R -f boost
         rm -f boost.tar.gz;;
     "Darwin")
