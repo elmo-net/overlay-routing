e94cb13093e6e7b4b7c31a34479f9435bc1de12b
Hans Meyer <hamster.of.dev@gmail.com>
2015-04-15 23:01:32 +0200

    Download and build boost on Linux

--- scripts/get-boost.sh
+++ scripts/get-boost.sh
@@ -7,10 +7,12 @@ case $os in
         echo "Downloading boost from official sources"
         wget -q -O boost.tar.gz http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz/download  \
             && echo "unpacking to ./boost/ (will be deleted afterwards)" && mkdir boost \
-            && tar --strip-components=1 boost -xzf boost.tar.gz \
-            && echo "Bootstrapping boost" && sudo boost/bootstrap.sh # let boost do its install routine
-        rm -R -f boost
-        rm -f boost.tar.gz;;
+            && tar -C boost -xzf boost.tar.gz \
+            && echo "Bootstrapping boost" && cd boost/boost_1_57_0 && sudo ./bootstrap.sh && ./b2 \
+            && cd ../../
+        sudo rm -R -f boost # edited by sudo, hence needs privileges
+        rm -f boost.tar.gz
+    ;;
     "Darwin")
         echo on MacOS;;
     *)
